d9b2849648a31b5050f747f9bf56822a06337c47
krishna-ts <krishna.m@talentsprint.com>
2024-07-04 19:28:11 +0530

    Wrong time showing issue resolved and time zone name changed

--- src/course-home/dates-tab/timeline/Day.jsx
+++ src/course-home/dates-tab/timeline/Day.jsx
@@ -67,9 +67,6 @@ const Day = ({
           const title = showLink ? (<u><a href={item.link} className="text-reset">{item.title}</a></u>) : item.title;
           const available = item.learnerHasAccess && (item.link || !isLearnerAssignment(item));
           const textColor = available ? 'text-primary-700' : 'text-gray-500';
-          console.log(item);
-          console.log("!!!");
-          console.log("item",item);
           return (
             <div key={item.title + item.date} className={classNames(textColor, 'small pb-1')} data-testid="dates-item">
               <div>
@@ -79,10 +76,9 @@ const Day = ({
                   {showDueDateTime && (
                     <span>
                       <span className="mx-1">due</span>
-                      <span className="mx-1">item.date</span>
                       <FormattedTime
                         value={item.date}
-                        timeZoneName="short"
+                        timeZoneName="long"
                         {...timezoneFormatArgs}
                       />
                     </span>
