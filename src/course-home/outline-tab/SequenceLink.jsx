125b5f16ba8523d927785939122a6254235fa306
manoj.p3 <manoj.p@talentsprint.com>
2024-09-30 16:26:45 +0530

    24 hrs instaed of 12 hrs section level time

--- src/course-home/outline-tab/SequenceLink.jsx
+++ src/course-home/outline-tab/SequenceLink.jsx
@@ -48,6 +48,7 @@ const SequenceLink = ({
         assignmentDue: (
           <FormattedTime
             key={`${id}-due`}
+            hour12={false}
             day="numeric"
             month="short"
             year="numeric"
