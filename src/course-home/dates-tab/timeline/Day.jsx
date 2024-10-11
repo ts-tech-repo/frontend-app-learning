50355771e6d2ed286c500395e6b47f64ff72ec3d
Chandana3008 <74036324+Chandana3008@users.noreply.github.com>
2024-10-11 15:54:20 +0530

    Debugging

--- src/course-home/dates-tab/timeline/Day.jsx
+++ src/course-home/dates-tab/timeline/Day.jsx
@@ -36,6 +36,9 @@ const Day = ({
 
   const { color, badges } = getBadgeListAndColor(date, intl, null, items);
 
+  console.log(first)
+  console.log(last)
+
   return (
     <li className="dates-day pb-4" data-testid="dates-day">
       {/* Top Line */}
