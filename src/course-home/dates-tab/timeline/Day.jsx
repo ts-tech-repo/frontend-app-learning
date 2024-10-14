f3283bbe525aa92a9b175ceb9beaddc13dc796aa
Chandana3008 <74036324+Chandana3008@users.noreply.github.com>
2024-10-14 15:45:53 +0530

    Debugging

--- src/course-home/dates-tab/timeline/Day.jsx
+++ src/course-home/dates-tab/timeline/Day.jsx
@@ -47,7 +47,7 @@ const Day = ({
       {!first && <div className="dates-line-top border-1 border-left border-gray-900 bg-gray-900" />}
 
       {/* Dot */}
-      <div className={classNames(color, 'dates-dot border border-gray-900')} style={(color == "custom-date-styling") ? myStyle : ""}/>
+      <div className={classNames(color, 'dates-dot border border-gray-900')} style={(color === "custom-date-styling") ? myStyle : ""}/>
 
       {/* Bottom Line */}
       {!last && <div className="dates-line-bottom border-1 border-left border-gray-900 bg-gray-900" />}
