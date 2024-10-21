5667b410c87395bc7e9f0fc9962435a3331701c1
sabid-ansari <113017359+sabid-ansari@users.noreply.github.com>
2024-10-21 15:31:45 +0530

    Merged timezone issue fixed

--- src/course-home/dates-tab/timeline/Day.jsx
+++ src/course-home/dates-tab/timeline/Day.jsx
@@ -23,6 +23,7 @@ const Day = ({
   intl,
   items,
   last,
+  unparsedDate,
 }) => {
   const {
     courseId,
@@ -76,7 +77,7 @@ const Day = ({
       <div className="d-inline-block ml-3 pl-2">
         <div className="row w-100 m-0 mb-1 align-items-center text-primary-700" data-testid="dates-header">
           <FormattedDate
-            value={date}
+            value={unparsedDate}
             day="numeric"
             month="short"
             weekday="short"
