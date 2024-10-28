d35769d032a846a5a8b3789a97cd61f0ebb2da6f
sabid.a <sabid.a@talentsprint.com>
2024-10-28 15:44:12 +0530

    Aman: Added feedback column in progress page

--- src/course-home/progress-tab/grades/detailed-grades/DetailedGradesTable.jsx
+++ src/course-home/progress-tab/grades/detailed-grades/DetailedGradesTable.jsx
@@ -41,6 +41,15 @@ const DetailedGradesTable = ({ intl }) => {
 
       return (
         <div className="my-3" key={`${chapter.displayName}-grades-table`}>
+          <style>
+              {`
+              .edx-datatable-table {
+                  table-layout: fixed !important;
+                  max-width: 100% !important;
+              }
+              `}
+          </style>
+
           <DataTable
             data={detailedGradesData}
             itemCount={detailedGradesData.length}
@@ -65,7 +74,7 @@ const DetailedGradesTable = ({ intl }) => {
               },
             ]}
           >
-            <DataTable.Table style={{ tableLayout: 'fixed !important', maxWidth: '100% !important' }} />
+            <DataTable.Table className="edx-datatable-table" />
           </DataTable>
         </div>
       );
