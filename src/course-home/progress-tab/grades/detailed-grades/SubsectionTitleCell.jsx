36f0dd8c4e561643e163eb409b007fb6e8209291
sabid.a <sabid.a@talentsprint.com>
2024-10-30 14:42:16 +0530

    to debug

--- src/course-home/progress-tab/grades/detailed-grades/SubsectionTitleCell.jsx
+++ src/course-home/progress-tab/grades/detailed-grades/SubsectionTitleCell.jsx
@@ -45,7 +45,7 @@ const SubsectionTitleCell = ({ intl, subsection }) => {
   return (
     <Collapsible.Advanced>
       <Row className="w-100 m-0">
-        {/* #SA || hidedProblemScoreSection
+        {/* #SA || hidedProblemScoreSection*/
         <Collapsible.Trigger
           className="mr-1 position-absolute"
           aria-label={intl.formatMessage(messages.problemScoreToggleAltText, { subsectionTitle: displayName })}
@@ -54,7 +54,7 @@ const SubsectionTitleCell = ({ intl, subsection }) => {
           <Collapsible.Visible whenClosed><Icon src={ArrowDropDown} /></Collapsible.Visible>
           <Collapsible.Visible whenOpen><Icon src={ArrowDropUp} /></Collapsible.Visible>
         </Collapsible.Trigger>
-        */}
+        }
         <span className="small d-inline pl-1">
           {gradesFeatureIsFullyLocked || subsection.learnerHasAccess ? ''
             : (
@@ -82,7 +82,7 @@ const SubsectionTitleCell = ({ intl, subsection }) => {
           )}
         </span>
       </Row>
-      {/* #SA || hidedProblemScoreSection
+      {/* #SA || hidedProblemScoreSection*/
       <Collapsible.Body className="d-flex w-100">
         <div className="col">
           { subsection.override && (
@@ -100,7 +100,7 @@ const SubsectionTitleCell = ({ intl, subsection }) => {
           <ProblemScoreDrawer problemScores={problemScores} subsection={subsection} />
         </div>
       </Collapsible.Body>
-      */}
+      }
     </Collapsible.Advanced>
   );
 };
