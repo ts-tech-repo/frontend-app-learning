90dd0559bcea87e7f195526c25dd8cb73fc76b3c
sabid.a <sabid.a@talentsprint.com>
2024-11-04 12:34:28 +0530

    Showing letter_grade if present

--- src/course-home/progress-tab/ProgressTab.jsx
+++ src/course-home/progress-tab/ProgressTab.jsx
@@ -65,7 +65,7 @@ const ProgressTab = () => {
           {!wideScreen && <CertificateStatus />}
           {/* <CourseGrade /> */}
           <div className={`grades my-4 p-4 rounded raised-card ${applyLockedOverlay}`} aria-hidden={gradesFeatureIsFullyLocked}>
-            { letterGradeExists && <GradeSummary /> }
+            { !letterGradeExists && <GradeSummary /> }
             <DetailedGrades />
           </div>
         </div>
