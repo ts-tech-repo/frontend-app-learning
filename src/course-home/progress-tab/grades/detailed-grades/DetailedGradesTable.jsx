2221ed32c8da0c4f2f5f1866d93379c401d194a0
Aman-K-Ray <aman.k@talentsprint.com>
2024-11-28 10:25:37 +0530

    update ui for score column

--- src/course-home/progress-tab/grades/detailed-grades/DetailedGradesTable.jsx
+++ src/course-home/progress-tab/grades/detailed-grades/DetailedGradesTable.jsx
@@ -49,7 +49,7 @@ const DetailedGradesTable = ({ intl }) => {
         const shouldTruncate = feedbackText.length > 15 && !isExpanded;
         return {
           subsectionTitle: <SubsectionTitleCell subsection={subsection} />,
-          score: <span className={subsection.learnerHasAccess ? '' : 'greyed-out'}>{subsection.letterGrade ? subsection.letterGrade : subsection.numPointsEarned.toFixed(2)}{subsection.letterGrade ? '' : (isLocaleRtl ? '\\' : '/')}{subsection.letterGrade ? '' : subsection.numPointsPossible.toFixed(2)}</span>,
+          score: <span id="score-column" className={subsection.learnerHasAccess ? '' : 'greyed-out'}>{subsection.letterGrade ? subsection.letterGrade : subsection.numPointsEarned.toFixed(2)}{subsection.letterGrade ? '' : (isLocaleRtl ? '\\' : '/')}{subsection.letterGrade ? '' : subsection.numPointsPossible.toFixed(2)}</span>,
           feedback: (
             <div>
               <span id="feedback-column" className={subsection.learnerHasAccess ? (isExpanded ? 'feedback-expanded' : 'feedback-truncated') : 'greyed-out'}>
