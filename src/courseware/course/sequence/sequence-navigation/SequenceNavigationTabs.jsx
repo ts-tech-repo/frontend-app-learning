70756b0343946fe25fb83b577b75b6284c3ac7d0
sohail.m <sohail.m@talentsprint.com>
2024-12-17 08:36:27 +0530

    debugging

--- src/courseware/course/sequence/sequence-navigation/SequenceNavigationTabs.jsx
+++ src/courseware/course/sequence/sequence-navigation/SequenceNavigationTabs.jsx
@@ -23,16 +23,7 @@ const SequenceNavigationTabs = ({
 
   return (
     <div style={{ flexBasis: '100%', minWidth: 0 }}>
-      {shouldDisplayDropdown === true || unitCount > 12 ? (
-        <>
-          <SequenceNavigationDropdown
-            unitId={unitId}
-            onNavigate={onNavigate}
-            showCompletion={showCompletion}
-            unitIds={unitIds}
-          />
-        </>
-      ) : (
+      {unitCount < 14 ? (
         <div className="sequence-navigation-tabs-container" ref={containerRef}>
           <div
             className="sequence-navigation-tabs d-flex flex-grow-1"
@@ -49,7 +40,17 @@ const SequenceNavigationTabs = ({
             ))}
           </div>
         </div>
-      )}
+      ) : (shouldDisplayDropdown === true || unitCount > 12) ? (
+        <>
+        {console.log("coming here")}
+          <SequenceNavigationDropdown
+            unitId={unitId}
+            onNavigate={onNavigate}
+            showCompletion={showCompletion}
+            unitIds={unitIds}
+          />
+        </>
+      ) : null}
     </div>
   );
 };
