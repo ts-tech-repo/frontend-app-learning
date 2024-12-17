923df6ee952997b1d076841098fa1057f911071e
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2024-12-17 14:41:42 +0530

    text changes Noticeboard

--- src/course-home/outline-tab/widgets/CourseTools.jsx
+++ src/course-home/outline-tab/widgets/CourseTools.jsx
@@ -69,7 +69,7 @@ const CourseTools = ({ intl }) => {
           <li key={courseTool.analyticsId} className="small">
             <a href={courseTool.url} onClick={() => logClick(courseTool.analyticsId)}>
               <FontAwesomeIcon icon={renderIcon(courseTool.analyticsId)} className="mr-2" fixedWidth />
-              {courseTool.title}
+              {courseTool.title === 'Updates' ? 'Noticeboard' : courseTool.title}
             </a>
           </li>
         ))}
