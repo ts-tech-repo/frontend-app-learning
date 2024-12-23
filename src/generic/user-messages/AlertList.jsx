b663a1229c79b2f889b8b6a7d42b276833db7926
dattatadepalli <datta.t@talentsprint.com>
2024-12-23 14:53:43 +0530

    Update AlertList.jsx

--- src/generic/user-messages/AlertList.jsx
+++ src/generic/user-messages/AlertList.jsx
@@ -1,31 +1,31 @@
-import React, { useContext, useCallback, Suspense } from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext, useCallback, Suspense } from "react";
+import PropTypes from "prop-types";
 
-import UserMessagesContext from './UserMessagesContext';
-import Alert from './Alert';
+import UserMessagesContext from "./UserMessagesContext";
+import Alert from "./Alert";
 
-const AlertList = ({
-  topic, className, customAlerts, customProps,
-}) => {
+const AlertList = ({ topic, className, customAlerts, customProps }) => {
   const { remove, messages } = useContext(UserMessagesContext);
   const getAlertComponent = useCallback(
     (code) => (customAlerts[code] !== undefined ? customAlerts[code] : Alert),
-    [customAlerts],
+    [customAlerts]
   );
 
-  const topicMessages = messages.filter(message => !topic || message.topic === topic);
+  const topicMessages = messages.filter(
+    (message) => !topic || message.topic === topic
+  );
   if (topicMessages.length === 0) {
     return null;
   }
 
   return (
     <div className={className}>
-      {topicMessages.map(message => {
+      {topicMessages.map((message) => {
         const AlertComponent = getAlertComponent(message.code);
-        console.log(message, message?.text,AlertComponent, "debugging")
+        console.log(message, message?.text, AlertComponent, "debugging");
         return (
           <Suspense key={message.id} fallback={null}>
-            <AlertComponent
+            {/* <AlertComponent
               type={message.type}
               dismissible={message.dismissible}
               onDismiss={() => remove(message.id)}
@@ -33,7 +33,7 @@ const AlertList = ({
               {...customProps}
             >
               {message.text}
-            </AlertComponent>
+            </AlertComponent> */}
           </Suspense>
         );
       })}
@@ -45,11 +45,7 @@ AlertList.propTypes = {
   className: PropTypes.string,
   topic: PropTypes.string,
   customAlerts: PropTypes.objectOf(
-    PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.func,
-      PropTypes.node,
-    ]),
+    PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.node])
   ),
   // eslint-disable-next-line react/forbid-prop-types
   customProps: PropTypes.object,
