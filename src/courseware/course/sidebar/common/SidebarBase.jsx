a5c0dd529c2ae091e46f0a214842ac66fc1cfa3e
sohail.m <sohail.m@talentsprint.com>
2024-12-24 15:44:38 +0530

    debugging

--- src/courseware/course/sidebar/common/SidebarBase.jsx
+++ src/courseware/course/sidebar/common/SidebarBase.jsx
@@ -35,25 +35,29 @@ const SidebarBase = ({
 
   useEventListener('message', receiveMessage);
 
-  useEffect(() => {
-    const handleSequenceNavigationClick = (event) => {
-      if (event.target.closest('.previous-button, .next-button, #courseware-sequenceNavigation .btn-link')) {
-        toggleSidebar(null);
-      }
-    };
+  // useEffect(() => {
+  //   const handleSequenceNavigationClick = (event) => {
+  //     if (event.target.closest('.previous-button, .next-button, #courseware-sequenceNavigation .btn-link')) {
+  //       toggleSidebar(null);
+  //     }
+  //   };
 
-    document.addEventListener('click', handleSequenceNavigationClick);
+  //   document.addEventListener('click', handleSequenceNavigationClick);
 
-    return () => {
-      document.removeEventListener('click', handleSequenceNavigationClick);
-    };
-  }, [toggleSidebar]);
+  //   return () => {
+  //     document.removeEventListener('click', handleSequenceNavigationClick);
+  //   };
+  // }, [toggleSidebar]);
   
 
   const { unitId,  courseId } = useContext(SidebarContext);
   useEffect(() => {    
     setTimeout(() => toggleSidebar(null), 1000);
   }, [unitId,  courseId]);
+  
+  useEffect(() => {    
+    toggleSidebar(null);
+  }, []);
 
   return (
     <section
