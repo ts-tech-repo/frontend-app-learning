ef4d08e35e406a8d7ab106aa10bd8c514fc381bb
sohail.m <sohail.m@talentsprint.com>
2024-12-24 10:20:03 +0530

    debugging

--- src/courseware/course/sidebar/common/SidebarBase.jsx
+++ src/courseware/course/sidebar/common/SidebarBase.jsx
@@ -28,6 +28,11 @@ const SidebarBase = ({
 
   useEffect(() => {
     setIsHidden(true);
+    const button = document.querySelector('.notification-btn[aria-label="Show discussions tray"]');
+    
+    if (button) {
+      setIsHidden(false);
+    }
   }, []);
 
   const receiveMessage = useCallback(({ data }) => {
