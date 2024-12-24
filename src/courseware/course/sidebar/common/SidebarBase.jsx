70a4a3b9c1d3ff12d927af3754f084ce205c5993
sohail.m <sohail.m@talentsprint.com>
2024-12-24 10:46:08 +0530

    temp reverting changes

--- src/courseware/course/sidebar/common/SidebarBase.jsx
+++ src/courseware/course/sidebar/common/SidebarBase.jsx
@@ -24,16 +24,6 @@ const SidebarBase = ({
     currentSidebar,
   } = useContext(SidebarContext);
 
-  const [isHidden, setIsHidden] = useState(true);
-
-  useEffect(() => {
-    setIsHidden(true);
-    const button = document.querySelector('.notification-btn[aria-label="Show discussions tray"]');
-    
-    if (button) {
-      button.onclick = () => setIsHidden(false);
-    }
-  }, []);
 
   const receiveMessage = useCallback(({ data }) => {
     const { type } = data;
@@ -45,12 +35,21 @@ const SidebarBase = ({
 
   useEventListener('message', receiveMessage);
 
+  // useEffect(() => {
+  //   const button = document.querySelector('.notification-btn[aria-label="Show discussions tray"]');
+    
+  //   if (button) {
+  //     button.click();
+  //   }
+  // }, []);
+  
+
   return (
     <section
       className={classNames('ml-0 ml-lg-4 border border-light-400 rounded-sm h-auto align-top', {
         'bg-white m-0 border-0 fixed-top vh-100 rounded-0': shouldDisplayFullScreen,
         'min-vh-100': !shouldDisplayFullScreen,
-        'd-none': currentSidebar !== sidebarId || isHidden,
+        'd-none': currentSidebar !== sidebarId,
       }, className)}
       data-testid={`sidebar-${sidebarId}`}
       style={{ width: shouldDisplayFullScreen ? '100%' : width }}
