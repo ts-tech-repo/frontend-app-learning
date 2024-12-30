fdacf9a1a610e8851c7c005ed4a84e4ec07cc7ea
dattatadepalli <datta.t@talentsprint.com>
2024-12-30 10:49:56 +0530

    Update CourseTabsNavigation.jsx

--- src/course-tabs/CourseTabsNavigation.jsx
+++ src/course-tabs/CourseTabsNavigation.jsx
@@ -1,42 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import classNames from 'classnames';
+import React from "react";
+import PropTypes from "prop-types";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 
-import messages from './messages';
-import Tabs from '../generic/tabs/Tabs';
+import messages from "./messages";
+import Tabs from "../generic/tabs/Tabs";
 
-const CourseTabsNavigation = ({
-  activeTabSlug, className, tabs, intl,
-}) => (
-  <div id="courseTabsNavigation" className={classNames('course-tabs-navigation', className)}>
-    <div className="container-xl">
-      <Tabs
-        className="nav-underline-tabs"
-        aria-label={intl.formatMessage(messages.courseMaterial)}
-      >
-        {tabs.map(({ url, title, slug }) => (
-          <a
-            key={slug}
-            className={classNames('nav-item flex-shrink-0 nav-link', { active: slug === activeTabSlug })}
-            href={url}
-          >
-            {title}
-          </a>
-        ))}
-      </Tabs>
+const CourseTabsNavigation = ({ activeTabSlug, className, tabs, intl }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      id="courseTabsNavigation"
+      className={classNames("course-tabs-navigation", className)}
+    >
+      <div className="container-xl">
+        <Tabs
+          className="nav-underline-tabs"
+          aria-label={intl.formatMessage(messages.courseMaterial)}
+        >
+          {tabs.map(({ url, title, slug }) => (
+            <p
+              key={slug}
+              className={classNames("nav-item flex-shrink-0 nav-link", {
+                active: slug === activeTabSlug,
+              })}
+              onClick={() => navigate(url)}
+              // href={url}
+            >
+              {title}
+            </p>
+          ))}
+        </Tabs>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 CourseTabsNavigation.propTypes = {
   activeTabSlug: PropTypes.string,
   className: PropTypes.string,
-  tabs: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    slug: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  })).isRequired,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   intl: intlShape.isRequired,
 };
 
