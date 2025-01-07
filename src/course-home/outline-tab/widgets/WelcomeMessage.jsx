acc5f221b523f7e7c1373f8e4ba7bf4bfd9d2b19
dattatadepalli <datta.t@talentsprint.com>
2025-01-07 15:47:32 +0530

    server error changes

--- src/course-home/outline-tab/widgets/WelcomeMessage.jsx
+++ src/course-home/outline-tab/widgets/WelcomeMessage.jsx
@@ -18,7 +18,7 @@ const WelcomeMessage = ({ courseId, intl }) => {
 
   const [display, setDisplay] = useState(true);
 
-  const shortWelcomeMessageHtml = truncate(welcomeMessageHtml, 100, { byWords: true, keepWhitespaces: true });
+  const shortWelcomeMessageHtml = welcomeMessageHtml && truncate(welcomeMessageHtml, 100, { byWords: true, keepWhitespaces: true });
   const messageCanBeShortened = shortWelcomeMessageHtml && shortWelcomeMessageHtml?.length < welcomeMessageHtml?.length;
   const [showShortMessage, setShowShortMessage] = useState(messageCanBeShortened);
   const dispatch = useDispatch();
