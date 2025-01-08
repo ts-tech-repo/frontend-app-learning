9e5242312cac083ef37bf43ecf2f61035845883c
sohail.m <sohail.m@talentsprint.com>
2025-01-08 13:32:32 +0530

    updated selector

--- src/courseware/course/sidebar/common/SidebarBase.jsx
+++ src/courseware/course/sidebar/common/SidebarBase.jsx
@@ -45,7 +45,7 @@ const SidebarBase = ({
 
   useEffect(() => {
     const handleSequenceNavigationClick = (event) => {
-      if (event.target.closest('.previous-button, .next-button, #courseware-sequenceNavigation .btn-link, .previous-btn')) {
+      if (event.target.closest('.previous-button, .next-button, #courseware-sequenceNavigation .btn-link, .previous-btn, li[data-testid="breadcrumb-item"]')) {
         toggleSidebar(null);
       }
     };
