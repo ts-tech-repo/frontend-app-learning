21a18944a1a1ee9c5c6fd76828c7dac394fc35a8
dattatadepalli <datta.t@talentsprint.com>
2025-01-15 11:14:52 +0530

    debugging

--- src/alerts/course-start-alert/hooks.js
+++ src/alerts/course-start-alert/hooks.js
@@ -44,7 +44,7 @@ export function useCourseStartMasqueradeBanner(courseId, tab) {
   const {
     isMasquerading,
   } = useModel('courseHomeMeta', courseId);
-  console.log(IsStartDateInFuture(courseId), "courseHomeMeta")
+  console.log(IsStartDateInFuture(courseId), useModel('courseHomeMeta', courseId), "courseHomeMeta")
   const isVisible = isMasquerading && tab === 'progress' && (IsStartDateInFuture(courseId) || false);
 
   // const payload = useMemo(() => ({
