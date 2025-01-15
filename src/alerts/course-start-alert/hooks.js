67ccb6aceaf86ba35630dea6612769e00872ae38
dattatadepalli <datta.t@talentsprint.com>
2025-01-15 11:24:22 +0530

    Update hooks.js

--- src/alerts/course-start-alert/hooks.js
+++ src/alerts/course-start-alert/hooks.js
@@ -66,7 +66,7 @@ export function useCourseStartMasqueradeBanner(courseId, tab) {
 
   useAlert(isVisible, {
     code: 'clientCourseStartMasqueradeBanner',
-    payload,
+    payload: {courseId},
     topic: 'instructor-toolbar-alerts',
   });
 
