6b06ddc675d79c5ab68ac7158953396a1b0aa9c3
dattatadepalli <datta.t@talentsprint.com>
2025-01-15 10:52:41 +0530

    Update hooks.js

--- src/alerts/course-start-alert/hooks.js
+++ src/alerts/course-start-alert/hooks.js
@@ -48,6 +48,8 @@ export function useCourseStartMasqueradeBanner(courseId, tab) {
   //   courseId,
   // }), [courseId]);
 
+  console.log(isVisible, "isVisible Payload")
+
   useEffect(() => {
     console.log(courseId, "courseIdcourseId")
   }, [courseId]);
