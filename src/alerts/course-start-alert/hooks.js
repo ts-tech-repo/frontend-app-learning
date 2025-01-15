03ca3444c6faeddfcf7271c2e74a586298ed4335
dattatadepalli <datta.t@talentsprint.com>
2025-01-15 13:43:19 +0530

    Update hooks.js

--- src/alerts/course-start-alert/hooks.js
+++ src/alerts/course-start-alert/hooks.js
@@ -5,11 +5,10 @@ import { useModel } from '../../generic/model-store';
 const CourseStartAlert = React.lazy(() => import('./CourseStartAlert'));
 const CourseStartMasqueradeBanner = React.lazy(() => import('./CourseStartMasqueradeBanner'));
 
-function IsStartDateInFuture(courseId) {
-  console.log('courseHomeMeta:', useModel('courseHomeMeta', courseId));
-  const {
-    start,
-  } = useModel('courseHomeMeta', courseId) || {};
+function IsStartDateInFuture(courseId, start) {
+  // const {
+  //   start,
+  // } = useModel('courseHomeMeta', courseId) || {};
   // if(start){
     const today = new Date();
   const startDate = new Date(start);
@@ -43,9 +42,10 @@ function useCourseStartAlert(courseId) {
 export function useCourseStartMasqueradeBanner(courseId, tab) {
   const {
     isMasquerading,
+    start
   } = useModel('courseHomeMeta', courseId) || {};
   console.log(IsStartDateInFuture(courseId), useModel('courseHomeMeta', courseId), "courseHomeMeta")
-  const isVisible = isMasquerading && tab === 'progress' ? IsStartDateInFuture(courseId) : false;
+  const isVisible = isMasquerading && tab === 'progress' ? IsStartDateInFuture(courseId, start) : false;
 
   // const payload = useMemo(() => ({
   //   courseId,
