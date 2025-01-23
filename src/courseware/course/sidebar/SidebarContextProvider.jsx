5ba53030d7f843db5090f6c5e076aa069af5bdf5
sohail.m <sohail.m@talentsprint.com>
2025-01-23 17:50:02 +0530

    debugging

--- src/courseware/course/sidebar/SidebarContextProvider.jsx
+++ src/courseware/course/sidebar/SidebarContextProvider.jsx
@@ -36,8 +36,13 @@ const SidebarProvider = ({
     // Switch to new sidebar or hide the current sidebar
     setCurrentSidebar(sidebarId === currentSidebar ? null : sidebarId);
   const discussionIcon = document.querySelector('.discussion-section');
-  discussionIcon.classList.toggle('d-none');
-  discussionIcon.classList.toggle('d-block');
+  if (discussionIcon.classList.contains('d-none')) {
+    discussionIcon.classList.remove('d-none');
+    discussionIcon.classList.add('d-block');
+  } else {
+    discussionIcon.classList.remove('d-block');
+    discussionIcon.classList.add('d-none');
+  }
   }, [currentSidebar]);
 
   const contextValue = useMemo(() => ({
