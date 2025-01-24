f80d104567dc0d725683604a85672b013838b036
sohail.m <sohail.m@talentsprint.com>
2025-01-24 10:51:32 +0530

    debugging

--- src/courseware/course/sequence/Unit/ContentIFrame.jsx
+++ src/courseware/course/sequence/Unit/ContentIFrame.jsx
@@ -36,6 +36,7 @@ const ContentIFrame = ({
   title,
 }) => {
   const [iframeClass, setIframeClass] = useState("");
+  const [isNoInternet, setIsNoInternet] = useState(!navigator.onLine);
 
   const { handleIFrameLoad, hasLoaded, iframeHeight, showError } =
     hooks.useIFrameBehavior({
@@ -46,6 +47,20 @@ const ContentIFrame = ({
     });
 
   const { modalOptions, handleModalClose } = hooks.useModalIFrameData();
+
+  useEffect(() => {
+    const handleOnlineStatus = () => {
+      setIsNoInternet(!navigator.onLine);
+    };
+
+    window.addEventListener("online", handleOnlineStatus);
+    window.addEventListener("offline", handleOnlineStatus);
+
+    return () => {
+      window.removeEventListener("online", handleOnlineStatus);
+      window.removeEventListener("offline", handleOnlineStatus);
+    };
+  }, []);
 
   useEffect(() => {
     const checkIframeContent = () => {
@@ -105,8 +120,10 @@ const ContentIFrame = ({
         // custom error message for iframe
         (showError ? (
           <div className="error_msg fade alert-content alert alert-danger show">
-            <span>🛈 </span> There seems to be a network issue. Please check your
-            connection and try again.
+            <span>🛈 </span>{" "}
+            {isNoInternet
+              ? "There seems to be no internet. Please check your connection and try again."
+              : "We are unable to handle your request right now. We will be back very soon. Please try again later."}
           </div>
         ) : (
           <PageLoading srMessage={loadingMessage} />
