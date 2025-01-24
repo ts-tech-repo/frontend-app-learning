b79aff66fd2d9fad86b81263d6eeff253f4cb70b
sohail.m <sohail.m@talentsprint.com>
2025-01-24 16:11:10 +0530

    debugging

--- src/courseware/course/sidebar/common/SidebarBase.jsx
+++ src/courseware/course/sidebar/common/SidebarBase.jsx
@@ -36,9 +36,34 @@ const SidebarBase = ({
 
   useEventListener('message', receiveMessage);
 
-  // useEffect(() => {
-  //   document.querySelector('.is-playing .video-player video').click();
-  // });  
+  useEffect(() => {
+    const navigationElements = document.querySelectorAll(
+      '.previous-button, .next-button, #courseware-sequenceNavigation .btn-link, .previous-btn, li[data-testid="breadcrumb-item"]'
+    );
+    const handleClick = () => {
+      const iframe = document.querySelector('#unit-iframe');
+      if (iframe) {
+        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        console.log(iframeDoc);
+        const videoElement = iframeDoc.querySelector('.is-playing .video-player video');
+        console.log(videoElement);
+        if (videoElement) {
+          videoElement.click();
+        } 
+      }
+    };
+
+    navigationElements.forEach(element => {
+      element.addEventListener('click', handleClick);
+    });
+
+    return () => {
+      navigationElements.forEach(element => {
+        element.removeEventListener('click', handleClick);
+      });
+    };
+  },[]);
+   
   
 
   // useEffect(() => {
