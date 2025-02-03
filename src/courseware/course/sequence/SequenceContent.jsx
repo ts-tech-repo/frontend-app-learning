76777c7e2195d43eb0a70343448d690d280ea9a8
venkata-s123 <venkata.s@talentsprint.com>
2025-02-03 15:04:29 +0530

    added support email

--- src/courseware/course/sequence/SequenceContent.jsx
+++ src/courseware/course/sequence/SequenceContent.jsx
@@ -58,7 +58,10 @@ const SequenceContent = ({
     return (
       <p className="text-center py-5 mx-auto" style={{ maxWidth: '30em' }}>
         {/* {intl.formatMessage(messages.loadFailure)} */}
-        The content has been marked hidden, for further information please contact support team {SUPPORT_EMAIL}.
+        The content has been marked hidden. For further information, please contact the support team at{' '}
+        <a href={`mailto:${SUPPORT_EMAIL}`} className="text-blue-500 underline">
+          {SUPPORT_EMAIL}
+        </a>.
       </p>
     );
   }
