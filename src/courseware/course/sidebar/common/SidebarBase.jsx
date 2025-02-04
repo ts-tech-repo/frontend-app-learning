6727569e40c08970810f3b87cb2349473aa7c967
manoj.p3 <manoj.p@talentsprint.com>
2025-02-04 16:38:54 +0530

    Close icon is not working in Discussion form in unit level #1 print logs

--- src/courseware/course/sidebar/common/SidebarBase.jsx
+++ src/courseware/course/sidebar/common/SidebarBase.jsx
@@ -37,62 +37,27 @@ const SidebarBase = ({
   useEventListener('message', receiveMessage);
 
   useEffect(() => {
-    const navigationElements = document.querySelectorAll(
-      '.previous-button, .next-button, #courseware-sequenceNavigation .btn-link, .previous-btn, li[data-testid="breadcrumb-item"]'
-    );
-  
-    const handleClick = () => {
-      const iframe = document.querySelector('#unit-iframe');
-      if (iframe) {
-        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-        if (iframeDoc) {
-          const videoElement = iframeDoc.querySelector('.is-playing .video-player video');
-          if (videoElement) {
-            const pauseButton = iframeDoc.querySelector('.pause'); 
-            if (pauseButton) {
-              const clickEvent = new MouseEvent('click', {
-                bubbles: true,
-                cancelable: true,
-                view: window,
-              });
-              pauseButton.dispatchEvent(clickEvent); 
-            } 
-          }
-        }
+    const iframe = document.querySelector("iframe");
+    if (!iframe) return;
+
+    const handleLoad = () => {
+      const button = iframe.contentDocument?.querySelector("button.spinner-dimentions");
+      if (button) {
+        button.addEventListener("click", () => toggleSidebar(null));
       }
     };
-  
-    navigationElements.forEach(element => {
-      element.addEventListener('click', handleClick);
-    });
-  
+
+    iframe.addEventListener("load", handleLoad);
+
     return () => {
-      navigationElements.forEach(element => {
-        element.removeEventListener('click', handleClick);
-      });
+      iframe.removeEventListener("load", handleLoad);
+      const button = iframe.contentDocument?.querySelector("button.spinner-dimentions");
+      if (button) {
+        button.removeEventListener("click", () => toggleSidebar(null));
+      }
     };
-  }, []);  
+  }, [toggleSidebar]);
   
-
-  // useEffect(() => {
-  //   const handleSequenceNavigationClick = (event) => {
-  //     if (event.target.closest('.previous-button, .next-button, #courseware-sequenceNavigation .btn-link, .previous-btn, li[data-testid="breadcrumb-item"]')) {
-  //       toggleSidebar(null);
-  //     }
-  //   };
-
-  //   document.addEventListener('click', handleSequenceNavigationClick);
-
-  //   return () => {
-  //     document.removeEventListener('click', handleSequenceNavigationClick);
-  //   };
-  // }, [toggleSidebar]);
-  
-
-  // const { unitId,  courseId } = useContext(SidebarContext);
-  // useEffect(() => {    
-  //   toggleSidebar(null);
-  // }, [unitId,  courseId]);
 
   return (
     <section
