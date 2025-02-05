a515157dd4e52e9fefece64f60cb44244e806b08
manoj.p3 <manoj.p@talentsprint.com>
2025-02-05 10:21:55 +0530

    Close icon is not working in Discussion form in unit level #12 Debugging

--- src/courseware/course/sidebar/SidebarContextProvider.jsx
+++ src/courseware/course/sidebar/SidebarContextProvider.jsx
@@ -32,12 +32,14 @@ const SidebarProvider = ({
   }, [courseId]);
 
   const toggleSidebar = useCallback((sidebarId) => {
-    // Switch to new sidebar or hide the current sidebar
-    setCurrentSidebar(sidebarId === currentSidebar ? null : sidebarId);
-  const discussionIcon = document.querySelector('.discussion-section');
-  discussionIcon.classList.toggle('d-none');
-  discussionIcon.classList.toggle('d-block');
+    const discussionIcon = document.querySelector('.discussion-section');
+    const isActive = sidebarId === currentSidebar;
+    
+    setCurrentSidebar(isActive ? null : sidebarId);
+    discussionIcon.classList.toggle('d-none', isActive);
+    discussionIcon.classList.toggle('d-block', !isActive);
   }, [currentSidebar]);
+  
 
   const contextValue = useMemo(() => ({
     toggleSidebar,
