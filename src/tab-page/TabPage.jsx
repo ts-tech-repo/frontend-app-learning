5fd5f85230b4180d0f6ac5954015df682706c84f
dattatadepalli <datta.t@talentsprint.com>
2025-02-06 15:45:47 +0530

    Update TabPage.jsx

--- src/tab-page/TabPage.jsx
+++ src/tab-page/TabPage.jsx
@@ -1,93 +1,104 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
-import Footer from '@edx/frontend-component-footer';
-import { Toast } from '@edx/paragon';
-import { LearningHeader as Header } from '@edx/frontend-component-header';
-import PageLoading from '../generic/PageLoading';
-import { getAccessDeniedRedirectUrl } from '../shared/access';
-import { useModel } from '../generic/model-store';
+import Footer from "@edx/frontend-component-footer";
+import { Toast } from "@edx/paragon";
+import { LearningHeader as Header } from "@edx/frontend-component-header";
+import PageLoading from "../generic/PageLoading";
+import { getAccessDeniedRedirectUrl } from "../shared/access";
+import { useModel } from "../generic/model-store";
 
-import genericMessages from '../generic/messages';
-import messages from './messages';
-import LoadedTabPage from './LoadedTabPage';
-import { setCallToActionToast } from '../course-home/data/slice';
-import LaunchCourseHomeTourButton from '../product-tours/newUserCourseHomeTour/LaunchCourseHomeTourButton';
+import genericMessages from "../generic/messages";
+import messages from "./messages";
+import LoadedTabPage from "./LoadedTabPage";
+import { setCallToActionToast } from "../course-home/data/slice";
+import LaunchCourseHomeTourButton from "../product-tours/newUserCourseHomeTour/LaunchCourseHomeTourButton";
 
 const TabPage = ({ intl, ...props }) => {
-  const {
-    activeTabSlug,
-    courseId,
-    courseStatus,
-    metadataModel,
-  } = props;
-  const {
-    toastBodyLink,
-    toastBodyText,
-    toastHeader,
-  } = useSelector(state => state.courseHome);
+  const { activeTabSlug, courseId, courseStatus, metadataModel } = props;
+  const { toastBodyLink, toastBodyText, toastHeader } = useSelector(
+    (state) => state.courseHome
+  );
   const dispatch = useDispatch();
-  const {
-    courseAccess,
-    number,
-    org,
-    start,
-    title,
-  } = useModel('courseHomeMeta', courseId);
+  const { courseAccess, number, org, start, title } = useModel(
+    "courseHomeMeta",
+    courseId
+  );
 
-  console.log(courseStatus, "courseStatuscourseStatus")
-
-  if (courseStatus === 'denied') {
-    const redirectUrl = getAccessDeniedRedirectUrl(courseId, activeTabSlug, courseAccess, start);
+  if (courseStatus === "denied") {
+    const redirectUrl = getAccessDeniedRedirectUrl(
+      courseId,
+      activeTabSlug,
+      courseAccess,
+      start
+    );
     if (redirectUrl) {
-      return (<Navigate to={redirectUrl} replace key={redirectUrl} />);
+      return <Navigate to={redirectUrl} replace key={redirectUrl} />;
     }
   }
 
   return (
     <>
-      {['loaded', 'denied'].includes(courseStatus) && (
+      {["loaded", "denied"].includes(courseStatus) && (
         <>
           <Toast
-            action={toastBodyText ? {
-              label: toastBodyText,
-              href: toastBodyLink,
-            } : null}
+            action={
+              toastBodyText
+                ? {
+                    label: toastBodyText,
+                    href: toastBodyLink,
+                  }
+                : null
+            }
             closeLabel={intl.formatMessage(genericMessages.close)}
-            onClose={() => dispatch(setCallToActionToast({ header: '', link: null, link_text: null }))}
-            show={!!(toastHeader)}
+            onClose={() =>
+              dispatch(
+                setCallToActionToast({
+                  header: "",
+                  link: null,
+                  link_text: null,
+                })
+              )
+            }
+            show={!!toastHeader}
           >
             {toastHeader}
           </Toast>
-          {metadataModel === 'courseHomeMeta' && (<LaunchCourseHomeTourButton srOnly />)}
+          {metadataModel === "courseHomeMeta" && (
+            <LaunchCourseHomeTourButton srOnly />
+          )}
         </>
       )}
 
       <Header courseOrg={org} courseNumber={number} courseTitle={title} />
 
-      {courseStatus === 'loading' && (
+      {courseStatus === "loading" && (
         <PageLoading srMessage={intl.formatMessage(messages.loading)} />
       )}
 
-      {['loaded', 'denied'].includes(courseStatus) && (
+      {["loaded", "denied"].includes(courseStatus) && (
         <LoadedTabPage {...props} />
       )}
 
       {/* courseStatus 'failed' and any other unexpected course status. */}
-      {(!['loading', 'loaded', 'denied', 'badGateway'].includes(courseStatus)) && (
-        <p className="text-center py-5 mx-auto" style={{ maxWidth: '30em' }}>
+      {!["loading", "loaded", "denied", "badGateway"].includes(
+        courseStatus
+      ) && (
+        <p className="text-center py-5 mx-auto" style={{ maxWidth: "30em" }}>
           {/* {intl.formatMessage(messages.failure)} */}
-          There seems to be a network issue. Please check your connection and try again.
+          There seems to be a network issue. Please check your connection and
+          try again.
         </p>
       )}
 
-      {(['badGateway'].includes(courseStatus)) && (
-        <p className="text-center py-5 mx-auto" style={{ maxWidth: '30em' }}>
+      {["badGateway"].includes(courseStatus) && (
+        <p className="text-center py-5 mx-auto" style={{ maxWidth: "30em" }}>
           {/* {intl.formatMessage(messages.failure)} */}
-          We are unable to handle your request right now. We will be back very soon. Please try again later.
+          There seems to be a network issue. Please check your connection and
+          try again.
         </p>
       )}
       <Footer />
